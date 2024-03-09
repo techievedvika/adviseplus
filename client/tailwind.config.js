8c9eee6f6624cfcd6ca2df9cbfd5f74c40294912
techievedvika <techievedvika>
2024-03-09 11:11:37 +0530

    changes updated

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'primaryone':'#ff4a17',
+        'primarytwo':'#14212b',
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
